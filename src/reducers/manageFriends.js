3dd35cd33c28dc8b4180b2dab6b596291bd96a0c
davidkorff <davidkorff@gmail.com>
2017-02-06 16:41:44 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+	if (action.type === 'ADD_FRIEND') {
+		return Object.assign({}, state, {friends: [...state.friends, action.payload]} )
+	} else if (action.type === 'REMOVE_FRIEND') {
+		let answer = []
+		for (let i = 0; i < state.friends.length; i++) {
+			if (state.friends[i].id !== action.payload) {answer.push(state.friends[i])}
+		}
+		return Object.assign({}, state, {friends: answer} )
+	}
+	else {return state}
+
 }
